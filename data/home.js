6643d5e15e6a745133beabc736dccbc08ce01021
Rameen <rameen07@gmail.com>
2021-10-19 12:55:30 +0500

    rizwan potfolio

--- data/home.js
+++ data/home.js
@@ -1,18 +1,28 @@
 var data = {
-  site_logo: "./assets/png/john-doe.png",
-  name: "John Doe",
-  heading: "Hey, My name is John Doe",
-  summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis tempora explicabo quae quod deserunt eius sapiente",
+  site_logo: "./assets/png/logo.png",
+  name: "RAG Engineering Consultants",
+  heading: "",
+  summary: "",
 
   about_me: {
-    short: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis tempora explicabo quae quod deserunt eius sapiente",
-    detailed: `Hey! It's <strong>John Doe</strong> and I'm a <strong> Frontend Web Developer </strong> located in Los Angeles. I've done
-      <strong> remote </strong> projects for agencies, consulted for startups, and collaborated with talented people to create"
-      <strong>digital products </strong> for both business and consumer use.`,
-    skills: ['HTML', 'CSS3', 'PHP', 'JavaScript', 'React', 'SASS', 'GIT', 'Shopify', 'Wordpress', 'Google ADS', 'Facebook Ads', 'Android', 'IOS']
+    short: `We are a design consultant and contractor company. We provide architectural
+     and structural design and construction services. We also give 
+    turn key solution of projects.`,
+    detailed: `Rag Consultant Offer Elegant And Innovative Design Solutions To Meet
+    Contemporary Requirements. Users Derive As Much Pleasure From
+    Occupying Their Buildings As We Do In Designing. A Holistic Approach
+    Ensures That Buildings Are Stylish, Practical, Comfortable And In
+    Perfect Harmony With Their Environment.We Believe In Functional Elegance And
+    Responsiveness To A Site’s Physical Surroundings Keeping In Perspective
+    The Brief Of The Client And The Approved Budget Constraints. This
+    Formula Has Transpired Into Several Successful Developments That Now
+    Line An Emerging Modern Landscape In Pakistan - And We Are Proud To Be
+    Part Of This Renaissance`,
+    skills: ['Architechtural Design', 'Structural Design', 'Value Engineering', 'MEP Design', 'Master Planning',
+    'Construction Management', 'Contractor Services', 'Feasibility Studies', 'Cost Estimation']
   },
 
-  projects_subheading: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic facilis tempora explicabo quae quod deserunt eius sapiente",
+  projects_subheading: "Completed Projects",
 
   projects: [
     {
